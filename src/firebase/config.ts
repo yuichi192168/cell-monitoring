0db6a33c81c419723f799c37d77c3b8cae36d7f2
Aljay Plantado <plantadoaljay00@gmail.com>
2026-05-15 14:00:28 +0000

    move the firebase config to .env
    export const firebaseConfig = {
      apiK

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDUdvNiEfiUhA_6mz88jdWMtH-xk2Do_jA",
-  authDomain: "studio-5141186190-77dac.firebaseapp.com",
-  projectId: "studio-5141186190-77dac",
-  storageBucket: "studio-5141186190-77dac.firebasestorage.app",
-  messagingSenderId: "248576416012",
-  appId: "1:248576416012:web:7ade832e2ae48849a1d129"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
